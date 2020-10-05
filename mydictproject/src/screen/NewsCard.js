7d4d30f06bf0a62affaea459fbb208a5a80b1b43
FaziletKosure <faziletkosure1@gmail.com>
2020-10-05 02:20:49 +0200

    add cards

--- mydictproject/src/screen/NewsCard.js
+++ mydictproject/src/screen/NewsCard.js
@@ -1,19 +1,19 @@
 import React from 'react'
 import { View, Text,StyleSheet,Image } from 'react-native'
 import {
-    getTheme,
-    ...
+    getTheme
   } from 'react-native-material-kit';
 
 const NewsCard = ({data}) => {
     const theme = getTheme();
     return (
         <View>
-            <Image  source={{uri : base64Icon}}style={theme.cardImageStyle}/>
-            <Text   style={theme.cardActionStyle}></Text>
-            <Text  style={theme.cardContentStyle}></Text>
+            <Image style={theme.cardImageStyle}  source={{uri: data.imageUrl}}/>
+            <Text style={theme.cardTitleStyle}>{data.title}</Text>
+            <Text style={theme.cardContentStyle}>{data.description}</Text>
+            <Text>Fazilet</Text>
         </View>
     )
 }
 
-export default NewsCard;
+export { NewsCard};
--- mydictproject/src/screen/NewsScreen.js
+++ mydictproject/src/screen/NewsScreen.js
@@ -1,28 +1,31 @@
 import React from 'react';
-import { Text, StyleSheet, View, Button, TouchableOpacity,FlatList } from 'react-native';
-import NewsCard from './NewsCard'
-import data from './data';
+import { Text, StyleSheet, View, Button, TouchableOpacity,FlatList,Image } from 'react-native';
+import {NewsCard} from './NewsCard';
+import { data} from './data';
 import {
-    getTheme,
-    ...
+    getTheme
   } from 'react-native-material-kit';
 const NewsScreen=()=>{
     const theme = getTheme();
-    const renderNews=({item})=><NewsCard data={item}/>
+    const renderNews=({item})=><NewsCard data={item}/>;
+   
     return(
-        <View>
+        <>
             <FlatList
             data={data}
             renderItem={renderNews}
-            ListHeaderComponent={()=><Text {theme.cardTitleStyle}></Text>}
-            />
-
-           
-           
-        </View>
+            // keyExtractor={item => item.id}
+            ListHeaderComponent={() => <Text style={styles.header}>News</Text>}
+          
+            ListHeaderComponentStyle={{margin:25}}
+            />     
+        </> 
 
     )
     
 }
+const styles = StyleSheet.create({
+    header: {fontSize: 35, padding: 5, fontWeight: 'bold'},
+  });
 
 export default NewsScreen;
